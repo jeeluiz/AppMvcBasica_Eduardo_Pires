b919c132fa1e87cf0f73363425419143216244b4
Jeferson Santos <jeferson.santos@onfriday.com.br>
2022-04-06 10:01:37 -0300

    Add project files.

---
+++ AppMvcBasica/Models/Endereco.cs
@@ -0,0 +1,8 @@
+﻿namespace AppMvcBasica.Models
+{
+    public class Endereco : Entity 
+    {
+        public Guid FornecedorId { get; set; }
+        public string Logradouro { get; set; }
+    }
+}
---
+++ AppMvcBasica/Models/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Name { get; set; }    
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
